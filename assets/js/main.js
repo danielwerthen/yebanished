4d48e3fd934b6cc16b990cd4d7498b4df3ea2729
Daniel Werthén <danielwerthen@gmail.com>
2014-09-13 15:08:53 +0200

    Lovely

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,15 @@
 ;(function(){
 
-			// Menu settings
-			$('#menuToggle, .menu-close').on('click', function(){
-				$('#menuToggle').toggleClass('active');
-				$('body').toggleClass('body-push-toleft');
-				$('#theMenu').toggleClass('menu-open');
-			});
+  // Menu settings
+  $('#menuToggle, .menu-close').on('click', function(){
+    $('#menuToggle').toggleClass('active');
+    $('body').toggleClass('body-push-toleft');
+    $('#theMenu').toggleClass('menu-open');
+  });
+
+  $(function () {
+    $('a.fluid-image').fluidbox();
+  });
 
 
 })(jQuery)
